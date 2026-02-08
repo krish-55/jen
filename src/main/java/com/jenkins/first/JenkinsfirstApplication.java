7f2b64516e051bea64dcde741a60d36bbb7978ea
Krish <krishstylishstar@gmail.com>
2026-02-08 08:46:34 +0530

    first commit

---
+++ src/main/java/com/jenkins/first/JenkinsfirstApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/first/JenkinsfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
